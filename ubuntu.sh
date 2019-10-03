843c2ee5220e3893bd225375e73921d5b4daba97
Daniel Connelly <Danc2@pdx.edu>
2019-10-02 22:05:45 -0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install git-core -y
 sudo apt-get install fish -y # the greatest shell in the world
+# TODO use swapspace commands to install 1gb swap space.
