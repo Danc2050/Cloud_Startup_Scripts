cb03e78f66771ec212f3ec3e2a6e9fbececc841a
Daniel Connelly <Danc2@pdx.edu>
2019-10-02 22:32:08 -0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,4 +3,12 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install git-core -y
 sudo apt-get install fish -y # the greatest shell in the world
+sudo apt-get install tmux -y
+sudo apt-get install python3-pip -y 
+sudo apt-get install python3 python-dev python3-dev \ # other useful python things if you are a dev
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python-pip
 # TODO use swapspace commands to install 1gb swap space.
+
+# TODO -- Pull github link and clone from commandline?
