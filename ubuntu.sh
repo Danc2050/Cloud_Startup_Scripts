ae778e83b9ac8c0de33324876050888f311e6aad
Daniel Connelly <Danc2@pdx.edu>
2019-10-02 22:32:48 -0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,6 +5,7 @@ sudo apt-get install git-core -y
 sudo apt-get install fish -y # the greatest shell in the world
 sudo apt-get install tmux -y
 sudo apt-get install python3-pip -y 
+sudo pip3 install --upgrade setuptools. # Some Python3 applications require this
 sudo apt-get install python3 python-dev python3-dev \ # other useful python things if you are a dev
      build-essential libssl-dev libffi-dev \
      libxml2-dev libxslt1-dev zlib1g-dev \
