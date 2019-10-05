8e7a924367ea52f9951e58a87f239bff5c71827f
Daniel Connelly <Danc2@pdx.edu>
2019-10-05 15:22:43 -0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
+
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install git-core -y
 sudo apt-get install fish -y # the greatest shell in the world
 sudo apt-get install tmux -y
 sudo apt-get install python3-pip -y 
-sudo pip3 install --upgrade setuptools. # Some Python3 applications require this
-sudo apt-get install python-dev python3-dev \ # other useful python things if you are a dev
-     build-essential libssl-dev libffi-dev \
-     libxml2-dev libxslt1-dev zlib1g-dev \
-     python-pip
-# TODO use swapspace commands to install 1gb swap space.
-
-# TODO -- Pull github link and clone from commandline?
+sudo pip3 install --upgrade setuptools # Some Python3 applications require this
+sudo apt-get install python3 python-dev python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip -y
+#sudo pip3 install mythril -y
+	# other useful python things if you are a dev
+     # TODO use swapspace commands to install 1gb swap space.
+     # TODO -- Pull github link and clone from commandline?
